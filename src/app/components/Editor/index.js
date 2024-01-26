73e134a5863c8ee24f6806167ae2a9abc17b1dd2
Thofire <46273054+Thofire@users.noreply.github.com>
2024-01-26 12:57:30 -0800

    update

--- src/app/components/Editor/index.js
+++ src/app/components/Editor/index.js
@@ -3,12 +3,19 @@ import { createEditor } from "slate"
 import withEmbeds from "./Elements/withEmbeds"
 import NoteInput from "./NoteInput"
 import ToolBarEditor from "./ToolBarEditor"
-import React,{ useState } from "react"
-
+import React, { useState } from "react"
+//import api for backend, call User.CurrentStorage from mongodb and thne pass it through, update isAstChange 
 
 const Editor = (props) => {
-    const {children,initialValue} = props
+    const { children, initialValue } = props
     const [editor] = useState(() => withEmbeds(withReact(createEditor())))
+    // const initialValue = useMemo(() =>
+    //     JSON.parse(localStorage.getItem('content'))
+    //     || [{
+    //         type: 'paragraph', children: [{ text: 'A line of text in a paragraph.' }],   Call Mongodb and store
+    //     },
+    //     ],
+    //     [])
     return (
         <Slate
             editor={editor}
@@ -21,11 +28,11 @@ const Editor = (props) => {
                 if (isAstChange) {
                     // Save the value to Local Storage.
                     const content = JSON.stringify(value)
-                    localStorage.setItem('content', content)
+                    localStorage.setItem('content', content)    //change to mongodb
                 }
             }}
         >
-            {React.Children.map(children,child=> React.cloneElement(child,{ editor }))}
+            {React.Children.map(children, child => React.cloneElement(child, { editor }))}
         </Slate>
     )
 }
